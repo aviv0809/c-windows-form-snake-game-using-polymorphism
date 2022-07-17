ee41d47e04dc6e388f2ce025d1bc975aa728904b
aviv ben yacov <43266249+aviv0809@users.noreply.github.com>
2022-07-17 19:08:39 +0300

    Add files via upload

---
+++ snake/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    //this is the snake head class and the apple that the snake eat class
+    internal class Circle
+    {
+        public int X { get; set; } // this is a public int class called X
+        public int Y { get; set; } // this is a public int class called Y
+
+        public Circle()
+        {
+            // this function is resetting the X and Y to 0
+            X = 0;
+            Y = 0;
+        }
+    }
+}
